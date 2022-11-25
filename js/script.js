146e28973fb1d370f95c9478dfa0127858476c89
aahedi <aahedi@gmail.com>
2022-11-25 09:40:43 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,14 +117,21 @@ next_btn.onclick = () => {
 
 // getting questions and options from array
 function showQuetions(index) {
+	var opt_random = shuffle(questions[index].options);
     const que_text = document.querySelector(".que_text");
 
     //creating a new span and div tag for question and option and passing the value using array index
     let que_tag = '<span>' + que_numb + ". " + questions[index].question + '</span>';
+	
+    let option_tag = '<div class="option"><span>' + opt_random[0] + '</span></div>'
+        + '<div class="option"><span>' + opt_random[1] + '</span></div>'
+        + '<div class="option"><span>' + opt_random[2] + '</span></div>'
+        + '<div class="option"><span>' + opt_random[3] + '</span></div>';
+	/*	
     let option_tag = '<div class="option"><span>' + questions[index].options[0] + '</span></div>'
         + '<div class="option"><span>' + questions[index].options[1] + '</span></div>'
         + '<div class="option"><span>' + questions[index].options[2] + '</span></div>'
-        + '<div class="option"><span>' + questions[index].options[3] + '</span></div>';
+        + '<div class="option"><span>' + questions[index].options[3] + '</span></div>';*/
     que_text.innerHTML = que_tag; //adding new span tag inside que_tag
     option_list.innerHTML = option_tag; //adding new div tag inside option_tag
 
